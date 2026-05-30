10886343e60bca07860c57665960b6263a3ec1cc
aaryavikasboya <aaryavikasb@gmail.com>
2026-05-30 12:50:17 +0530

    configure Railway MySql databse connection

--- src/db/indexDB.js
+++ src/db/indexDB.js
@@ -1,22 +1,18 @@
 import mysql from "mysql2/promise";
 
-import { DB_NAME } from "../constants.js";
+// import { DB_NAME } from "../constants.js";
 export let db;
 export const connectDB = async () => {
     try {
         db = await mysql.createConnection({
-            host: "localhost",
+            host: process.env.DB_HOST,
             user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT
         })
-        await db.query(
-            `CREATE DATABASE IF NOT EXISTS ${DB_NAME}`
-        );
 
-        await db.query(
-            `USE ${DB_NAME}`
-        );
-        console.log(`\n MySql connected !! DB HOST ${db.config.host}`);
+        console.log(`\n MySql connected !! DB HOST ${process.env.DB_HOST}`);
         return db;
 
     } catch (error) {
